cb149d10f5574a6aea6068a8e3cc24b01eb3a21f
Jurgen Schatz <jurgen.schatz@standardbank.co.za>
2018-05-18 13:40:35 +0200

    FileChecker Channels

--- monitor/service.go
+++ monitor/service.go
@@ -4,6 +4,8 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/weAutomateEverything/fileMonitorService/fileChecker"
 	"log"
+	"sync"
+	"time"
 )
 
 type Service interface {
@@ -32,24 +34,66 @@ func NewService() Service {
 
 func statusCheck() *service {
 	s := &service{}
+
 	log.Println("File arrival confirmation commencing")
 	common := []string{"SE", "GL", "TXN", "DA", "MS", "EP", "VTRAN", "VOUT", "VISA_OUTGOING_MONET_TRANS_REPORT", "VISA_INCOMING_FILES_SUMMARY_REPORT", "TRANS_INPUT_LIST_", "VISA_INCOMING_MONET_TRANS_REPORT", "VISA_OUTGOING_FILES_SUMMARY_REPORT", "MC_INCOMING_MONET_TRANS_REPORT", "MC_OUTGOING_MONET_TRANS_REPORT", "RECON_REPORT", "MERCH_REJ_TRANS", "MC_OUTGOING_FILES_SUMMARY_REPORT", "MASTERCARD_ACKNOWLEDGEMENT_REPORT", "MC_INCOMING_FILES_SUMMARY_REPORT", ".001", ".002", ".003", ".004", ".005", ".006"}
+
+	var wg sync.WaitGroup
+
+	locationBuf := make(chan fileChecker.Service)
 	//Zimbabwe
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Zimbabwe","/mnt/zimbabwe", append(common)...))
+
+	wg.Add(8)
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Zimbabwe", "/mnt/zimbabwe", append(common)...)
+		wg.Done()
+	}()
 	//Zambia
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Zambia", "/mnt/zambiaprod", append(common)...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Zambia", "/mnt/zambiaprod", append(common)...)
+		wg.Done()
+	}()
 	//Ghana
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Ghana","/mnt/ghana", append(common, "MUL")...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Ghana", "/mnt/ghana", append(common, "MUL")...)
+		time.Sleep(10*time.Second)
+		wg.Done()
+	}()
 	//GhanaUSD
-	s.targets = append(s.targets, fileChecker.NewFileChecker("GhanaUSD","/mnt/ghanausd", append(common)...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("GhanaUSD", "/mnt/ghanausd", append(common)...)
+		wg.Done()
+	}()
 	//Botswana
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Botswana","/mnt/botswana", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Botswana", "/mnt/botswana", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...)
+		wg.Done()
+	}()
 	//Namibia
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Namibia","/mnt/namibia", append(common, "MUL", "INT00001", "INT00003", "INT00007", "SR00001", "SPTLSB_NA_", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Namibia", "/mnt/namibia", append(common, "MUL", "INT00001", "INT00003", "INT00007", "SR00001", "SPTLSB_NA_", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...)
+		wg.Done()
+	}()
 	//Malawi
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Malawi","/mnt/malawi", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Malawi", "/mnt/malawi", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...)
+		wg.Done()
+	}()
 	//Kenya
-	s.targets = append(s.targets, fileChecker.NewFileChecker("Kenya","/mnt/kenya", append(common)...))
+	go func() {
+		locationBuf <- fileChecker.NewFileChecker("Kenya", "/mnt/kenya", append(common)...)
+		wg.Done()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(locationBuf)
+	}()
+
+	for ch := range locationBuf {
+		s.targets = append(s.targets, ch)
+	}
+
 	status = s
 	return s
 }
