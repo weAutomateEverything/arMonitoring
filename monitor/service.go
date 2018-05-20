edc2d144ae09027b27404ef10ef6f7e166acd7f4
jlschatz <jurgen.schatz@gmail.com>
2018-05-20 19:16:22 +0200

    Refactor

--- monitor/service.go
+++ monitor/service.go
@@ -4,7 +4,6 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/weAutomateEverything/fileMonitorService/fileChecker"
 	"log"
-	"sync"
 )
 
 type Service interface {
@@ -26,51 +25,49 @@ func NewService() Service {
 		<-sched.Start()
 	}()
 
-	s := statusCheck()
+	statusCheck()
 
-	return s
+	return status
 }
 
-func statusCheck() *service {
-	s := &service{}
+func updateMap(locationMap map[string]map[string]bool) {
+
+	status.targets = append(status.targets, locationMap)
+}
+
+func statusCheck() {
 
 	log.Println("File arrival confirmation commencing")
 	common := []string{"SE", "GL", "TXN", "DA", "MS", "EP", "VTRAN", "VOUT", "VISA_OUTGOING_MONET_TRANS_REPORT", "VISA_INCOMING_FILES_SUMMARY_REPORT", "TRANS_INPUT_LIST_", "VISA_INCOMING_MONET_TRANS_REPORT", "VISA_OUTGOING_FILES_SUMMARY_REPORT", "MC_INCOMING_MONET_TRANS_REPORT", "MC_OUTGOING_MONET_TRANS_REPORT", "RECON_REPORT", "MERCH_REJ_TRANS", "MC_OUTGOING_FILES_SUMMARY_REPORT", "MASTERCARD_ACKNOWLEDGEMENT_REPORT", "MC_INCOMING_FILES_SUMMARY_REPORT", ".001", ".002", ".003", ".004", ".005", ".006"}
 
-	var wg sync.WaitGroup
-
 	//Zimbabwe
-	wg.Add(7)
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Zimbabwe", "/mnt/zimbabwe", append(common)...))
+		updateMap(fileChecker.NewFileChecker("Zimbabwe", "/mnt/zimbabwe", append(common)...))
 	}()
 	//Zambia
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Zambia", "/mnt/zambiaprod", append(common)...))
+		updateMap(fileChecker.NewFileChecker("Zambia", "/mnt/zambiaprod", append(common)...))
 	}()
 	//Ghana
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Ghana", "/mnt/ghana", append(common, "MUL")...))
+		updateMap(fileChecker.NewFileChecker("Ghana", "/mnt/ghana", append(common, "MUL")...))
 	}()
 	//GhanaUSD
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("GhanaUSD", "/mnt/ghanausd", append(common)...))
+		updateMap(fileChecker.NewFileChecker("GhanaUSD", "/mnt/ghanausd", append(common)...))
 	}()
 	//Botswana
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Botswana", "/mnt/botswana", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+		updateMap(fileChecker.NewFileChecker("Botswana", "/mnt/botswana", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
 	}()
 	//Namibia
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Namibia", "/mnt/namibia", append(common, "MUL", "INT00001", "INT00003", "INT00007", "SR00001", "SPTLSB_NA_", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+		updateMap(fileChecker.NewFileChecker("Namibia", "/mnt/namibia", append(common, "MUL", "INT00001", "INT00003", "INT00007", "SR00001", "SPTLSB_NA_", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
 	}()
 	//Malawi
 	go func() {
-		s.targets = append(s.targets,fileChecker.NewFileChecker("Malawi", "/mnt/malawi", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
+		updateMap(fileChecker.NewFileChecker("Malawi", "/mnt/malawi", append(common, "MUL", "DCI_OUTGOING_MONET_TRANS_REPORT", "DCI_TRANS_INPUT_LIST_")...))
 	}()
-
-	status = s
-	return s
 }
 
 func (s *service) StatusResults() []fileChecker.Service {
