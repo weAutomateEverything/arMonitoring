8fc68417d0186275f3f0f352276b244ecb1a779e
Jurgen Schatz <jurgen.schatz@standardbank.co.za>
2018-05-24 13:01:33 +0200

    Only 5 retries on failed share access

--- fileChecker/service.go
+++ fileChecker/service.go
@@ -2,11 +2,11 @@ package fileChecker
 
 import (
 	"fmt"
+	"github.com/matryer/try"
 	"log"
 	"os"
 	"strings"
 	"time"
-	"github.com/matryer/try"
 )
 
 type Service interface {
@@ -29,17 +29,22 @@ func NewFileChecker(name, mountpath string, files ...string) (string, map[string
 	log.Println(fmt.Sprintf("Now accessing %s share", name))
 
 	for _, x := range files {
-		value := s.pathToMostRecentFile(mountpath, x)
+		value, err := s.pathToMostRecentFile(mountpath, x)
+		if err != nil {
+			for _, file := range files {
+				s.fileStatus[file] = "unaccessable"
+			}
+			return s.locationName, s.fileStatus
+		}
 		s.fileStatus[x] = value
 	}
-	//s.fileStatusCollection[name] = s.fileStatus
 
 	log.Println(fmt.Sprintf("Completed file confirmation process on %s share", name))
 
 	return s.locationName, s.fileStatus
 }
 
-func (s *service) pathToMostRecentFile(dirPath, fileContains string) string {
+func (s *service) pathToMostRecentFile(dirPath, fileContains string) (string, error) {
 
 	var fileList []string
 
@@ -48,14 +53,14 @@ func (s *service) pathToMostRecentFile(dirPath, fileContains string) string {
 		var err error
 		fileList, err = s.GetFilesInPath(dirPath)
 		if err != nil {
-			log.Println(fmt.Sprintf("Failed to access %s. Trying again in 20 seconds", dirPath))
+			log.Println(fmt.Sprintf("Failed to access %s. Trying again in 2 seconds", dirPath))
 			time.Sleep(2 * time.Second)
 		}
 		return attempt < 5, err
 	})
 	if err != nil {
-		log.Println(fmt.Sprintf("Unable to access %s", dirPath))
-		return "unaccessable"
+		log.Println(fmt.Sprintf("Unable to access %s, Please confirm share mount access", dirPath))
+		return "", err
 	}
 
 	currentDate := time.Now().Format("20060102")
@@ -68,56 +73,56 @@ func (s *service) pathToMostRecentFile(dirPath, fileContains string) string {
 		cont := strings.Contains(file, fileContains)
 		recent := strings.Contains(file, currentDate)
 
-		if recent == true && cont == true && convertedTime.After(expectedTime){
-			return "late"
+		if recent == true && cont == true && convertedTime.After(expectedTime) {
+			return "late", nil
 		}
-		if recent == true && cont == true && convertedTime.Before(expectedTime){
-			return "received"
+		if recent == true && cont == true && convertedTime.Before(expectedTime) {
+			return "received", nil
 		}
 	}
-	return "notreceived"
+	return "notreceived", nil
 }
 
-func expectedFileArivalTime(file string)time.Time{
+func expectedFileArivalTime(file string) time.Time {
 
 	expectedTimes := map[string]string{
 
-		".001":                               "01:30:00",
-		".002":                               "01:30:00",
-		".003":                               "01:30:00",
-		".004":                               "01:30:00",
-		".005":                               "01:30:00",
-		".006":                               "01:30:00",
-		"DA":                                 "05:30:00",
-		"DCI_OUTGOING_MONET_TRANS_REPORT":    "05:30:00",
-		"DCI_TRANS_INPUT_LIST_":              "05:30:00",
-		"EP":                                 "07:30:00",
-		"GL":                                 "01:30:00",
-		"INT00001":                           "01:30:00",
-		"INT00003":                           "01:30:00",
-		"INT00007":                           "01:30:00",
-		"MASTERCARD_ACKNOWLEDGEMENT_REPORT":  "05:30:00",
-		"MC_INCOMING_FILES_SUMMARY_REPORT":   "05:30:00",
-		"MC_INCOMING_MONET_TRANS_REPORT":     "05:30:00",
-		"MC_OUTGOING_FILES_SUMMARY_REPORT":   "05:30:00",
-		"MC_OUTGOING_MONET_TRANS_REPORT":     "05:30:00",
-		"MERCH_REJ_TRANS":                    "05:30:00",
-		"MS":                                 "05:30:00",
-		"MUL":                                "01:30:00",
-		"RECON_REPORT":                       "05:30:00",
-		"SE":                                 "01:30:00",
-		"SPTLSB_NA_":                         "21:00:00",
-		"SR00001":                            "01:30:00",
-		"TRANS_INPUT_LIST_":                  "05:30:00",
-		"TXN":                                "01:30:00",
+		".001": "01:30:00",
+		".002": "01:30:00",
+		".003": "01:30:00",
+		".004": "01:30:00",
+		".005": "01:30:00",
+		".006": "01:30:00",
+		"DA":   "05:30:00",
+		"DCI_OUTGOING_MONET_TRANS_REPORT": "05:30:00",
+		"DCI_TRANS_INPUT_LIST_":           "05:30:00",
+		"EP":                                "07:30:00",
+		"GL":                                "01:30:00",
+		"INT00001":                          "01:30:00",
+		"INT00003":                          "01:30:00",
+		"INT00007":                          "01:30:00",
+		"MASTERCARD_ACKNOWLEDGEMENT_REPORT": "05:30:00",
+		"MC_INCOMING_FILES_SUMMARY_REPORT":  "05:30:00",
+		"MC_INCOMING_MONET_TRANS_REPORT":    "05:30:00",
+		"MC_OUTGOING_FILES_SUMMARY_REPORT":  "05:30:00",
+		"MC_OUTGOING_MONET_TRANS_REPORT":    "05:30:00",
+		"MERCH_REJ_TRANS":                   "05:30:00",
+		"MS":                                "05:30:00",
+		"MUL":                               "01:30:00",
+		"RECON_REPORT":                      "05:30:00",
+		"SE":                                "01:30:00",
+		"SPTLSB_NA_":                        "21:00:00",
+		"SR00001":                           "01:30:00",
+		"TRANS_INPUT_LIST_":                 "05:30:00",
+		"TXN":                               "01:30:00",
 		"VISA_INCOMING_FILES_SUMMARY_REPORT": "05:30:00",
 		"VISA_INCOMING_MONET_TRANS_REPORT":   "05:30:00",
 		"VISA_OUTGOING_FILES_SUMMARY_REPORT": "05:30:00",
 		"VISA_OUTGOING_MONET_TRANS_REPORT":   "05:30:00",
-		"VOUT":                               "07:30:00",
-		"VTRAN":                              "07:30:00",
-		"EP747":							  "07:30:00",
-		"CGNI":								  "07:30:00",
+		"VOUT":  "07:30:00",
+		"VTRAN": "07:30:00",
+		"EP747": "07:30:00",
+		"CGNI":  "07:30:00",
 	}
 
 	expectedTime := expectedTimes[file]
@@ -127,7 +132,7 @@ func expectedFileArivalTime(file string)time.Time{
 	return t
 }
 
-func convertTime(unconvertedTime string)time.Time{
+func convertTime(unconvertedTime string) time.Time {
 
 	t, err := time.Parse("15:04:05", unconvertedTime)
 	if err != nil {
