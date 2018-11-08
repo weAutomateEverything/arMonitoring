f6bbde5627ab898d144a5edf383acdbfea26da69
Schatz, Jurgen J <jurgen.schatz@standardbank.co.za>
2018-11-08 18:17:28 +0200

    Insert correct string

--- cyberArk/getCredentials.go
+++ cyberArk/getCredentials.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"fmt"
 )
 
 type Service interface {
@@ -58,7 +59,7 @@ func (s *service) GetCyberarkPassword() error {
 
 func (s *service) updateCedentialsFile() {
 
-	err := ioutil.WriteFile("/opt/app/creds", []byte("bolloks"), 0755)
+	err := ioutil.WriteFile("/opt/app/creds", []byte(fmt.Sprintf("username=%v\npassword=%v", s.Username, s.Password)), 0755)
 	if err != nil {
 		log.Printf("Failed writing to credentials file with the following error: %v", err)
 	}
